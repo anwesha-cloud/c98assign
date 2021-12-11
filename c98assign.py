468d9292937c1490458587142dcd2d5a7166b79d
anwesha-cloud <62639539+anwesha-cloud@users.noreply.github.com>
2021-12-11 12:32:44 +0530

    Add files via upload

---
+++ c98assign.py
@@ -0,0 +1,13 @@
+def swappingFileData():
+    file1=input("enter file1 name: ")
+    file2=input("enter file2 name: ")
+    with open (file1,"r") as a:
+        data_a=a.read()
+    with open (file2,"r") as b:
+        data_b=b.read()
+    with open(file1,"w") as a:
+        a.write(data_b)
+    with open(file2,"w") as b:
+        b.write(data_a)
+
+swappingFileData()
